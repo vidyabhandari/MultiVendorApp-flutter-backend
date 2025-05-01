9567ca47d0baf30d135a41859ad8b70b27d9c461
vidyabhandari <vidyabhandari2804@gmail.com>
2025-05-01 11:55:17 +0530

    oder controller created

---
+++ controllers/orderController.js
@@ -0,0 +1,47 @@
+const Oder = (require = "../models/Oder");
+
+module.exports = {
+  placeOrder: async (req, res) => {
+    const newOrder = new Order({
+      ...req.body,
+      userId: req.user.id,
+    });
+
+    try {
+      await newOrder.save();
+
+      const orderId = newOrder._id;
+      res.status(200).json({
+        status: true,
+        message: "Order placed successfully",
+        oderId: oderId,
+      });
+    } catch (error) {
+      res.status(500).json({ status: true, message: error.message });
+    }
+  },
+
+  getUserOders: async (req, res) => {
+    const userId = req.user.id;
+    const { paymentStatus, orderStatus } = req.query;
+
+    let query = { userId };
+    if (paymentStatus) {
+      query.paymentStatus = paymentStatus;
+    }
+
+    if (orderStatus === orderStatus) {
+      query.orderStatus = orderStatus;
+    }
+    try {
+      const orders = await Order.find(query).populate({
+        path: "orderItems.foodId",
+        select: "imageUrl title rating time",
+      });
+
+      res.status(200).json(orders);
+    } catch (error) {
+      res.status(500).json({ status: false, message: error.message });
+    }
+  },
+};
