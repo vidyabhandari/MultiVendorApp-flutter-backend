637d05354421fb135ea183191d4d4b6587a87db5
vidyabhandari <vidyabhandari9696@gmail.com>
2025-06-11 16:26:17 +0530

    fcm filed added to user model

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema(
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     otp: { type: String, required: true, default: "none" },
+    fcm: { type: String, required: true, default: "none" },
     password: { type: String, required: true },
     verification: { type: Boolean, required: false },
     phone: { type: String, default: "0123456789" },
